49cf4cc197f1e8253f569f6195be175da2adc9ae
Muhammad MUJTABA <mujtaba1582@gmail.com>
2024-06-16 10:39:36 +0500

    new

---
+++ Video 56/script.js
@@ -0,0 +1,15 @@
+console.log("hello i am conditional tuturial")
+
+
+let age=17;
+
+let grace=2;
+
+console.log(age**grace)
+
+if((age+grace)>18){
+    console.log("you can drive")
+}
+else{
+    console.log("you cannot drive")
+}
